64b6cf07870a5c170400a141318ec1dc029aca65
myrma <skolopedrion@gmail.com>
2016-05-28 18:40:49 +0200

    Implemented command line interface

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,8 @@ import Control.Monad.Except (throwError)
 
 import qualified Data.Map as Map
 
-import System.Environment (getArgs)
+import System.Console.ArgParser.SubParser
+import System.Console.ArgParser
 
 
 defaultEnv :: Scope Value
@@ -65,10 +66,37 @@ defaultEnv = Map.fromList [
 		printF = HLam $ \ arg -> lift (print arg) >> return UnitV
 
 
-main :: IO ()
-main = do
-	(path:_) <- getArgs
-	progAST  <- parseFile path
+data Action
+	= RunFile String
+	| RunExpr String
+
+commandLineParser :: CmdLnInterface Action
+commandLineParser = mkSubParserWithName "ZdS Acid Interpreter"
+  [ ("exec", mkDefaultApp
+      	(RunFile
+			`parsedBy` reqPos "path" `Descr` "The path of the file to execute")
+        "exec" `setAppDescr` "Interpret the given file")
+  , ("expr", mkDefaultApp
+      	(RunExpr
+	  		`parsedBy` reqPos "expr" `Descr` "The expression to execute")
+        "expr" `setAppDescr` "Run a given expression")
+  ]
+
+runAction :: Action -> IO ()
+runAction (RunFile path) = do
+	progAST <- parseFile path
 	case progAST of
-		Left err  -> print err
+		Left  err -> print err
 		Right ast -> runEval (evalProgram ast defaultEnv) >> return ()
+
+runAction (RunExpr expr) = do
+	case parseExpr expr of
+		Left  err -> print err
+		Right ast -> do
+			res <- runEval (evalExpr ast defaultEnv)
+			case res of
+				Right val -> print val
+				Left  err -> print err
+
+main :: IO ()
+main = runApp commandLineParser runAction
